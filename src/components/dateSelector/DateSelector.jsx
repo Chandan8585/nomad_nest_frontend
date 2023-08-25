9da2f93d7853973c790d48d6f9955d9a912a312b
Chandan8585 <chandanwingshr@gmail.com>
2023-08-25 21:44:02 +0530

    corrected routing of dateSelector.scss

--- src/components/dateSelector/DateSelector.jsx
+++ src/components/dateSelector/DateSelector.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import DatePicker from "react-datepicker"
 import "react-datepicker/dist/react-datepicker.css";
-import "./DateSelector.scss"
+import "./dateSelector.scss"
 const DateSelector = ({placeholder , checkInType}) => {
   return (
     <div>
