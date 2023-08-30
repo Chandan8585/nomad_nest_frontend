9d66acbce873d5f64732fee7169f478db4fef694
Chandan8585 <chandanwingshr@gmail.com>
2023-08-30 09:31:13 +0530

    added filter wrt rating

--- src/components/filters/rating/Rating.jsx
+++ src/components/filters/rating/Rating.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-const ratings = ["1", '2', '3','4', '5']
+import { useFilter } from '../../../context/filter-context'
+const ratings = ['1', '2', '3','4', '5']
 const Rating = () => {
+    const { ratingSelected, filterDispatch} = useFilter();
+    const handleRatingClick = (rating)=> {
+           filterDispatch({
+            type: "SELECTED_RATING",
+            payload: rating
+           })
+    }
   return (
     <div className="filter-container">
       <span className="filter-label">Ratings</span>
       <div className="d-flex align-center gap">
         {ratings.map((rating) => (
           <span
-            className="span-label aminity-count star d-flex align-center justify-center cursor-pointer on-hover"
+            className={`span-label aminity-count star d-flex align-center justify-center cursor-pointer on-hover ${ratingSelected === rating? "selected" : ""}` }
             key={rating}
-            
+            onClick={()=> handleRatingClick(rating)}
           >
             {rating} &Up
           </span>
