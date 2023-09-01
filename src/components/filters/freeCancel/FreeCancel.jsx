8089eeeed7d4da131bb1459a62a37ecfdfaf7702
Chandan8585 <chandanwingshr@gmail.com>
2023-09-01 19:37:07 +0530

    filter cancallable hotels added

--- src/components/filters/freeCancel/FreeCancel.jsx
+++ src/components/filters/freeCancel/FreeCancel.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import "./FreeCancel.scss"
+import { useFilter } from '../../../context/filter-context'
 const FreeCancel = () => {
+    const {filterDispatch, isCancellable} = useFilter();
+    console.log(isCancellable);
+    const handleInputChange= (event)=>{
+        filterDispatch({
+            type: "CANCELABLE",
+            payload : event.target.checked
+        })
+    }
   return (
     <div className="filter-container">
     <div className="d-flex align-center gap-larger">
       <span className="filter-label">Free Cancellation</span>
       <label className="slide">
         <input
-          type="checkbox"
+          type="checkbox" onChange={(event)=> handleInputChange(event)} value={isCancellable} checked={isCancellable}
         />
         <span className="slider round"></span>
       </label>
